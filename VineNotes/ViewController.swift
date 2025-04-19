7054331e36b24c37bc07ae2cb4207bf201606e7c
mozheng.sh <mozheng.sh@alibaba-inc.com>
2025-04-19 21:59:38 +0800

    Initial Commit

---
+++ VineNotes/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  VineNotes
+//
+//  Created by mozheng on 2025/4/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
